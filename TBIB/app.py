1f6a10a198b8e444f8c2108f49dd4df0ea9fe2cd
randomwalid <walid.madaoui2007@gmail.com>
2025-12-23 16:58:56 +0100

    Update app.py

--- TBIB/app.py
+++ TBIB/app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, session
-from extensions import db, migrate, login_manager
+from TBIB.extensions import db, migrate, login_manager
 
 def create_app():
     app = Flask(__name__)
